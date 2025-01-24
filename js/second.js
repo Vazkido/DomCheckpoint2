ecac478635bf4ac6bc1dfab22a29b8bc192afcca
valentine <vazkid02@gmail.com>
2025-01-24 13:43:06 -0500

    JS update

---
+++ js/second.js
@@ -0,0 +1,6 @@
+let col = document.getElementById('')
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    let clr = document.getElementById('color-box');
+    let chn = document.getElementById('change-color-btn');
+})
